e6b79f6a64d39683129e0553152ff48c6f8cbc61
Kip Cole <kipcole9@gmail.com>
2026-05-16 05:06:04 +1000

    Fix mix

--- mix.exs
+++ mix.exs
@@ -100,7 +100,7 @@ defmodule Localize.Inputs.MixProject do
       {:gettext, "~> 1.0", optional: true},
       {:ex_doc, "~> 0.30", only: [:dev, :release], runtime: false},
       {:dialyxir, "~> 1.4", only: :dev, runtime: false}
-    ] ++ maybe_json_polyfill() ++ maybe_cldr()
+    ] ++ maybe_json_polyfill()
   end
 
   defp maybe_json_polyfill do
