1a37eabb60a8df5ef521065882fc7dd98907512c
Kip Cole <kipcole9@gmail.com>
2026-05-16 05:04:34 +1000

    Add json_polyfill

--- mix.exs
+++ mix.exs
@@ -100,7 +100,15 @@ defmodule Localize.Inputs.MixProject do
       {:gettext, "~> 1.0", optional: true},
       {:ex_doc, "~> 0.30", only: [:dev, :release], runtime: false},
       {:dialyxir, "~> 1.4", only: :dev, runtime: false}
-    ]
+    ] ++ maybe_json_polyfill() ++ maybe_cldr()
+  end
+
+  defp maybe_json_polyfill do
+    if Code.ensure_loaded?(:json) do
+      []
+    else
+      [{:json_polyfill, "~> 0.2 or ~> 1.0"}]
+    end
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
